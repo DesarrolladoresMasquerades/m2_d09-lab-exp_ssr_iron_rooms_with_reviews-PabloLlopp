81d03d4ce3a365c1e3b92aa69fbcd894cffa4c56
Pablo López <p.lopezmunoz@gmail.com>
2022-02-18 09:33:14 +0100

    updated owner edit/delete

--- rooms-app/routes/rooms.js
+++ rooms-app/routes/rooms.js
@@ -14,14 +14,33 @@ const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 const { post } = require(".");
 
-router.route("/")
+
+
+router.route("/:id/details")
 .get((req, res)=> {
-    res.render("rooms/rooms")
+    
+    Room.findById(req.params.id)
+    .populate("owner")
+    .then((room)=>{
+        let roomOwner = false;
+        if(req.session.user && room.owner._id == req.session.user._id){
+            roomOwner = true;
+            res.render("rooms/room-details", {room, roomOwner})
+        }
+        res.render("rooms/room-details", room)
+    })
+    /*
+    .then((room)=> {
+        console.log(room.owner)
+        res.render("rooms/room-details", room)})
+        */
 })
+
 
 router.route("/create")
 .get(isLoggedIn, (req, res)=> {
     res.render("rooms/create-room")
+    .catch(err=> console.log(`There was an error accessing the create form: ${err}`));
 })
 .post((req, res)=> {
     const { name, description, imgUrl } = req.body;
@@ -29,7 +48,14 @@ router.route("/create")
 
     Room.create({name, description, imgUrl, owner: ownerId})
     .then(res.redirect("/rooms"))
+    .catch(err=> console.log(`There was an error creating a room: ${err}`));
+})
 
+router.route("/")
+.get((req, res)=> {
+    Room.find()
+    .then((rooms)=> res.render("rooms/rooms", {rooms}))
+    .catch(err=> console.log(`There was an error showing the rooms: ${err}`));
 })
 
 module.exports = router;
